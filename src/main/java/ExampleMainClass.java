5fe69f8897c61f0a5fb79c9e074125725fdc3629
YuraFarshyniou <farshyniou@gmail.com>
2022-08-07 18:32:11 +0300

    from pc with love

--- src/main/java/ExampleMainClass.java
+++ src/main/java/ExampleMainClass.java
@@ -4,8 +4,6 @@ public class ExampleMainClass {
     public static void main(String[] args) {
         Class<List> listClass = List.class;
         System.out.println(listClass.getConstructors().toString());
-        System.out.println("Hello From Honor!");
-
-        System.out.println("Some changes");
+        System.out.println("Hello From PC!");
     }
 }
