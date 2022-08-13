9d107db569dc6089bc12d76c430945e31c81c826
YuraFarshyniou <farshyniou@gmail.com>
2022-08-13 16:54:05 +0300

    Message

--- src/main/java/ExampleMainClass.java
+++ src/main/java/ExampleMainClass.java
@@ -3,28 +3,27 @@ import bean.Vehicle;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 public class ExampleMainClass {
-    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException,  InstantiationException, ClassNotFoundException {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException, ClassNotFoundException {
         Car someCar = Car.class.getDeclaredConstructor().newInstance();
         Class<? extends Car> carCl = someCar.getClass();
-         carCl.getDeclaredMethod("someMethod",String.class, int.class).invoke(someCar,"Ford",70);
+        carCl.getDeclaredMethod("someMethod", String.class, int.class).invoke(someCar, "Ford", 70);
 
 
         Car car = null;
         Class<?> carClass = Class.forName(Car.class.getName());
-        Class[]params = {Integer.class,Vehicle.class};
+        Class[] params = {Integer.class, Vehicle.class};
         Car vehicle = Car.class.getDeclaredConstructor().newInstance();
-        car = (Car) carClass.getDeclaredConstructor(params).newInstance(4,vehicle );
+        car = (Car) carClass.getDeclaredConstructor(params).newInstance(4, vehicle);
         car.setSeats(5);
         System.out.println(car.getSeats());
 
         Constructor<?>[] constructors = carClass.getConstructors();
-        for(Constructor constructor:constructors){
+        for (Constructor constructor : constructors) {
             System.out.println(constructor);
             Class[] parameterTypes = constructor.getParameterTypes();
-            for(Class param:parameterTypes){
+            for (Class param : parameterTypes) {
                 System.out.println(param);
             }
         }
