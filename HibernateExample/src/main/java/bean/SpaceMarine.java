bdde46b84e717498c0a65691db6c2113d36544c6
YuraFarshyniou <farshyniou@gmail.com>
2022-08-18 18:46:40 +0300

    Simple HIbernate project

---
+++ HibernateExample/src/main/java/bean/SpaceMarine.java
@@ -0,0 +1,58 @@
+package bean;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "space_marine")
+public class SpaceMarine {
+
+    @Id
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SpaceMarine that = (SpaceMarine) o;
+        return id == that.id && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "SpaceMarine{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
--- HibernateExample/src/main/java/hibernate_starter/HibernateMain.java
+++ HibernateExample/src/main/java/hibernate_starter/HibernateMain.java
@@ -1,7 +1,25 @@
 package hibernate_starter;
 
+import bean.SpaceMarine;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 public class HibernateMain {
     public static void main(String[] args) {
-        System.out.println("Hello Hibernate!!!");
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure()
+                .build();
+        SessionFactory factory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        Session session = factory.openSession();
+        SpaceMarine Loken = new SpaceMarine();
+        Loken.setName("Loken");
+        session.beginTransaction();
+        session.persist(Loken);
+        session.getTransaction().commit();
+        session.close();
+        factory.close();
     }
 }
