f4137765fdb8152088a3025491a25b4f114fbf62
YuraFarshyniou <farshyniou@gmail.com>
2023-02-08 17:24:44 +0300

    Forgot about one component

--- react_example/src/App.js
+++ react_example/src/App.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import './styles/App.css'
-function App() {
+import PostList from "./Components/PostList";
+import MyButton from "./Components/UI/button/MyButton";
+import MyInput from "./Components/UI/input/MyInput";
 
+function App() {
+    const [posts, setPosts] = useState([
+        {id: 55, title: "JavaScript", body: "JAvascript has a dick"},
+        {id: 76, title: "Java", body: "Cool language"},
+        {id: 999, title: "Python", body: "Fucking snake"}
+    ])
+    const [title, setTitle] = useState("");
+    const bodyInputRef = useRef();
+
+    const addNewPost = (e) => {
+        e.preventDefault();
+        console.log(bodyInputRef.current.value)
+    }
     return (
         <div className="App">
-            <div className={"post"}>
-                <div className={"post_content"}>
-                    <strong>1. JavaScript</strong>
-                    <div>
-                        Javascript - it's a programmer language.
-                    </div>
-                </div>
-                <div className={"post__btns"}>
-                    <button>Delete</button>
-                </div>
-            </div>
+            <form>
+                <MyInput value={title}
+                         onChange={e => setTitle(e.target.value)}
+                         type="text"
+                         placeholder="Post name"/>
+                <input ref={bodyInputRef} type="text"/>
+                {/*<MyInput*/}
+                {/*    ref={bodyInputRef}*/}
+                {/*    type="text"*/}
+                {/*    placeholder="Post description"/>*/}
+                <MyButton onClick={addNewPost}>Sometitle</MyButton>
+            </form>
+            <PostList posts={posts} title="Posts about JS"/>
+
         </div>
     );
 }
