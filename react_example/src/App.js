9903dbe54c22cb9da1c40c435a5d3cf3fdf94179
YuraFarshyniou <farshyniou@gmail.com>
2023-02-21 17:45:46 +0300

    1.14

--- react_example/src/App.js
+++ react_example/src/App.js
@@ -1,9 +1,8 @@
-import React, {useRef, useState} from 'react';
+import React, {useState} from 'react';
 import './styles/App.css'
 import PostList from "./Components/PostList";
-import MyButton from "./Components/UI/button/MyButton";
-import MyInput from "./Components/UI/input/MyInput";
 import PostForm from "./Components/PostForm";
+import {MySelect} from "./Components/UI/select/MySelect";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -18,11 +17,29 @@ function App() {
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
     }
+    const [selectedSort, setSelectedSort] = useState('')
+
+    const sortPosts = (sort) => {
+        setSelectedSort(sort);
+        setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+    }
 
     return (
         <div className="App">
             <PostForm create={createPost}/>
-            <PostList remove={removePost} posts={posts} title="Posts about JS"/>
+            <hr style={{margin: '15px 0'}}/>
+            <MySelect defaultValue={"Sorting"}
+                      value={selectedSort}
+                      onChange={sortPosts}
+                      options={[{value: 'title', name: "Name"},
+                          {value: 'body', name: "Description"}
+
+                      ]}
+            />
+            {posts.length
+                ? <PostList remove={removePost} posts={posts} title="Posts about JS"/>
+                : <h1 style={{textAlign: 'center'}}>Post list is empty!</h1>}
+
 
         </div>
     );
